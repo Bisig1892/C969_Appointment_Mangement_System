e929b8a3571ec9fef2ac7283d922843a6ede73e6
Tyler Bisig <bisig1892@gmail.com>
2024-10-10 12:52:14 -0700

    Added language on login for Mexico region. Converting login form text to spanish

--- C969_Appointment_Mangement_System/LoginForm.cs
+++ C969_Appointment_Mangement_System/LoginForm.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +16,33 @@ namespace C969_Appointment_Mangement_System
 {
     public partial class LoginForm : Form
     {
+
+        MySqlDataReader reader;
+        MySqlCommand cmd;
+
+
         public LoginForm()
         {
             InitializeComponent();
+            CheckLanguage();
+        }
 
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void CheckLanguage()
+        {
+            if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "MX")
+            {
+                this.Text = "Acceso";
+                loginLabel.Text = "Acceso";
+                usernameLabel.Text = "Nombre de usuario";
+                passwordLabel.Text = "Contraseña";
+                loginBtn.Text = "Acceso";
+                closeBtn.Text = "Cerca";
+            }
         }
     }
 }
