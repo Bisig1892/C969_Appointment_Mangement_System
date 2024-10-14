a0b5f6a6ffa148d23d80431495eaab0951f805d8
Tyler Bisig <bisig1892@gmail.com>
2024-10-14 16:24:58 -0700

    Main form customer table populated

--- C969_Appointment_Mangement_System/MainForm.Designer.cs
+++ C969_Appointment_Mangement_System/MainForm.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.appointmentsDGV = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CustomerDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsDGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // appointmentsDGV
+            // 
+            this.appointmentsDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.appointmentsDGV.Location = new System.Drawing.Point(12, 49);
+            this.appointmentsDGV.Name = "appointmentsDGV";
+            this.appointmentsDGV.Size = new System.Drawing.Size(557, 256);
+            this.appointmentsDGV.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Appointments";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 369);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Customers";
+            // 
+            // CustomerDGV
+            // 
+            this.CustomerDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CustomerDGV.Location = new System.Drawing.Point(12, 395);
+            this.CustomerDGV.Name = "CustomerDGV";
+            this.CustomerDGV.Size = new System.Drawing.Size(557, 256);
+            this.CustomerDGV.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1058, 693);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CustomerDGV);
+            this.Controls.Add(this.appointmentsDGV);
+            this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.appointmentsDGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CustomerDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView appointmentsDGV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView CustomerDGV;
     }
 }
--- C969_Appointment_Mangement_System/MainForm.cs
+++ C969_Appointment_Mangement_System/MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using C969_Appointment_Mangement_System.Database;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,34 @@ namespace C969_Appointment_Mangement_System
 {
     public partial class MainForm : Form
     {
+        MySqlDataReader reader;
+        MySqlCommand cmd;
+        MySqlConnection conn = DBConnection.conn;
+
+
         public MainForm()
         {
             InitializeComponent();
+            populateCustomerTable();
         }
+
+        private void populateCustomerTable()
+        {
+            cmd = new MySqlCommand("SELECT customer.customerId AS ID, customer.customerName AS Customer_Name, address.address AS Address, address.phone AS Phone, city.city AS City, country.country AS Country " +
+                                    "FROM customer JOIN address ON customer.addressId = address.addressId " +
+                                                  "JOIN city ON address.cityId = city.cityId " +
+                                                  "JOIN country ON city.countryId = country.countryId;", conn);
+            reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                var custTable = new BindingSource();
+                custTable.DataSource = reader;
+                CustomerDGV.DataSource = custTable;
+            }
+            reader.Close();
+        }
+
+
     }
 }
