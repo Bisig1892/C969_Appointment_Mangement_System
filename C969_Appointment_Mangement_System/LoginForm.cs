8b432d430cc980f3ddcc8057335445cd6966da77
Tyler Bisig <bisig1892@gmail.com>
2024-11-11 09:14:21 -0800

    Logging successful and unsucessful login attempts implemented

--- C969_Appointment_Mangement_System/LoginForm.cs
+++ C969_Appointment_Mangement_System/LoginForm.cs
@@ -14,6 +14,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace C969_Appointment_Mangement_System
 {
@@ -22,6 +24,7 @@ namespace C969_Appointment_Mangement_System
         MySqlDataReader reader;
         MySqlCommand cmd;
         MySqlConnection conn = DBConnection.conn;
+        MySqlDataAdapter adp;
 
         private readonly string userCulture = CultureInfo.CurrentCulture.Name;
         public static string username { get; set; }
@@ -30,7 +33,6 @@ namespace C969_Appointment_Mangement_System
         public LoginForm()
         {
             InitializeComponent();
-            //CheckLanguage();
         }
 
 
@@ -53,27 +55,57 @@ namespace C969_Appointment_Mangement_System
             string username = usernameTb.Text;
             string password = passwordTb.Text;
 
+            // log file file location C969_Appointment_Mangement_System\C969_Appointment_Mangement_System\bin\Debug
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs-Login.txt";
 
             try
             {
-                cmd = new MySqlCommand($"SELECT userId, userName, password FROM user WHERE userName = '{username}' AND password = '{password}'", conn);
-                DBConnection.startConnection();
-                using (reader = cmd.ExecuteReader())
-          
-                if (reader.HasRows)
+                cmd = new MySqlCommand($"SELECT userId, userName, password FROM user", conn);
+                MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+                bool loginSuccess = false;
+                string userId = "";
+                
+
+                foreach (DataRow dr in dt.Rows)
                 {
-                    if (userCulture == "es-MX")
+                    if (dr["userName"].ToString() == username && dr["password"].ToString() == password) 
                     {
-                        MessageBox.Show($"Inicio de sesión exitosa \n¡Bienvenido {username}!");
+                        loginSuccess = true;
+                        userId = dr["userId"].ToString();
+                        break;
                     }
                     else
                     {
-                        MessageBox.Show($"Login successful \nWelcome {username}!");
+                        loginSuccess = false;
                     }
-                    this.Hide();
-                    reader.Close();
+                }
 
-                    MainForm mainForm = new MainForm();
+                if (loginSuccess == true)
+                {
+                    if (File.Exists(filePath) == false)
+                    {
+                        File.Create(filePath).Dispose();
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append($"USER: {username} successfully logged in at {DateTime.UtcNow} UTC. \n");
+
+                        File.AppendAllText(filePath, sb.ToString());
+                        sb.Clear();
+                    }
+                    else if (File.Exists(filePath) == true)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append($"USER: {username} successfully logged in at {DateTime.UtcNow} UTC. \n");
+
+                        File.AppendAllText(filePath, sb.ToString());
+                        sb.Clear();
+                    }
+
+                    this.Hide();
+                    MainForm mainForm = new MainForm(userId);
                     mainForm.Show();
                 }
                 else
@@ -86,17 +118,70 @@ namespace C969_Appointment_Mangement_System
                     {
                         MessageBox.Show("Wrong passsword or username. Please try again.");
                     }
-                    usernameTb.Clear();
                     passwordTb.Clear();
-                    reader.Close();
+
+                    if (File.Exists(filePath) == false)
+                    {
+                        File.Create(filePath).Dispose();
+
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append($"FAILED login attempt with USER: {username} at {DateTime.UtcNow} UTC. \n");
+
+                        File.AppendAllText(filePath, sb.ToString());
+                        sb.Clear();
+                    }
+                    else if (File.Exists(filePath) == true)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append($"FAILED login attempt with USER: {username} at {DateTime.UtcNow} UTC. \n");
+
+                        File.AppendAllText(filePath, sb.ToString());
+                        sb.Clear();
+                    }
                 }
             }
             catch (Exception ex) 
             {
                 MessageBox.Show(ex.Message); 
             }
-            
+
         }
+
+        
+           
+
+        //private void upcomingAppt(string Id)
+        //{
+        //    string userId = Id;
+            
+        //    string appointmentAlert = $"SELECT * FROM appointment";
+        //    cmd = new MySqlCommand(appointmentAlert, conn);
+        //    adp = new MySqlDataAdapter(cmd);
+        //    DataTable dt = new DataTable();
+        //    adp.Fill(dt);
+
+        //    foreach(DataRow dr in dt.Rows)
+        //    {
+        //        if (dr["userId"].ToString() == userId)
+        //        {
+        //            var dBTime = DateTime.Parse(dr["start"].ToString()).ToLocalTime();
+        //            var localTime = DateTime.Now.ToLocalTime();
+
+        //            MessageBox.Show(dBTime.ToString(), localTime.ToString());
+        //            if (dBTime.Date == localTime.Date)
+        //            {
+        //                var timeDifferential = dBTime.Subtract(localTime);
+        //                if (timeDifferential.TotalMinutes <= 15)
+        //                {
+        //                    MessageBox.Show("Attention!\nThere is an appointment within the next 15 minutes.");
+        //                }
+        //            }
+        //        }
+        //    }
+
+
+
+        //}
 
         private void closeBtn_Click(object sender, EventArgs e)
         {
